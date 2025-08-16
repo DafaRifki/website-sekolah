bb6a19b5e687b99555c478a81cd201ac7da87278
MaulanaAdam <155249743+maulanaadam248@users.noreply.github.com>
2025-08-16 13:47:30 +0700

    fe:ubahan total

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from './components/ui/button'
+
+const App = () => {
+  return (
+    <div>
+      <Button variant={"destructive"}>klik saya</Button>
+    </div>
+  )
+}
+
+export default App
